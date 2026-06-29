171a8dc01da7da168a669a015fb71c159ca9326a
Spandan-m <meshramspandan28@gmail.com>
2026-06-29 11:25:33 +0530

    Done with react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
+
+import Landing from "./pages/Landing.jsx";
 
 function App() {
 
   return (
-    <>
+    <> 
+    <Routes>
+      <Route path="/" element={<Landing/>} />
+    </Routes>
     </>
   )
 }
