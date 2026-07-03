8f58aa9cf254d0be93337b7ca4f95796587fb3a5
Spandan-m <meshramspandan28@gmail.com>
2026-07-03 12:46:39 +0530

    done setup with auth

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,17 +1,30 @@
 import axiosInstance from "../api/axios";
 
 const register = async (userData) => {
-    const response = await axiosInstance.post("/auth/register",userData);
+  try {
+    const response = await axiosInstance.post("/auth/register", userData);
     return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to register user.",
+    );
+  }
 };
 
 const login = async (userData) => {
-    
+  try{
+    const response = await axiosInstance.post("/auth/login",userData);
+    return response.data;
+  }catch(error){
+    throw new Error(
+        error.response?.data?.message || "login failed."
+    );
+  }
 };
 
 const authService = {
-    register,
-    login
-}
+  register,
+  login,
+};
 
 export default authService;
