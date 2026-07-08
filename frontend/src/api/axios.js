f8cf609c05639584f9c48ac029b400235995f031
Spandan-m <meshramspandan28@gmail.com>
2026-07-08 15:02:27 +0530

    removed /api for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URI}/api`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URI}`,
   headers: {
     "Content-Type": "application/json",
   },
