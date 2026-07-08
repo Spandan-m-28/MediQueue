0f1aa4972085bbaad36e87dbcfe9b519f093dc53
Spandan-m-28 <155769282+Spandan-m-28@users.noreply.github.com>
2026-07-08 15:08:19 +0530

    Update axios baseURL to include '/api' path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URI}`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URI}/api`,
   headers: {
     "Content-Type": "application/json",
   },
